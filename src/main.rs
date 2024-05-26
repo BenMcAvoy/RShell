20479d1f4893767b84a5579520b2904679b4d5f4
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-05-26 02:39:41 +0100

    feat: add echo builtin

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ macro_rules! printnnl {($($arg:tt)*) => {print!($($arg)*);io::stdout().flush().u
 macro_rules! builtins_map {
     () => (std::collection::HashMap::new());
     ($($key:expr => $value:expr), + $(,)?) => ({
-        let mut map = std::collections::HashMap::new();
+        let mut map: std::collections::HashMap<&str, fn(Vec<&str>)> = std::collections::HashMap::new();
         $(map.insert($key, $value);)+
         map
     })
@@ -17,12 +17,18 @@ fn exit(args: Vec<&str>) {
     match args.len() {
         1 => std::process::exit(0),
         _ => std::process::exit(args[1].parse::<i32>().unwrap()),
-    }
+    };
+}
+
+fn echo(args: Vec<&str>) {
+    let args = args[1..].join(" ");
+    println!("{args}");
 }
 
 fn main() {
     let builtins = builtins_map! {
         "exit" => exit,
+        "echo" => echo,
     };
 
     loop {
