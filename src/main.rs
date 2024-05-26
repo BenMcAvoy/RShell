e358358a39c6380d925b1b6b4536c20ed8c072d9
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-05-26 02:28:16 +0100

    feat: implement exit builtin

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,26 @@ use std::collections::HashMap;
 use std::io::{self, Write};
 
 macro_rules! printnnl {($($arg:tt)*) => {print!($($arg)*);io::stdout().flush().unwrap();}}
+macro_rules! builtins_map {
+    () => (std::collection::HashMap::new());
+    ($($key:expr => $value:expr), + $(,)?) => ({
+        let mut map = std::collections::HashMap::new();
+        $(map.insert($key, $value);)+
+        map
+    })
+}
+
+fn exit(args: Vec<&str>) {
+    match args.len() {
+        1 => std::process::exit(0),
+        _ => std::process::exit(args[1].parse::<i32>().unwrap()),
+    }
+}
 
 fn main() {
-    let commands: HashMap<&str, Box<dyn Fn(Vec<&str>) -> ()>> = HashMap::new();
+    let builtins = builtins_map! {
+        "exit" => exit,
+    };
 
     loop {
         printnnl!("$ ");
@@ -15,8 +32,11 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        match commands.get(input.trim()) {
-            Some(command) => command(vec![]),
+        let args = input.trim().split_whitespace().collect::<Vec<&str>>();
+        let command = args[0];
+
+        match builtins.get(command) {
+            Some(command) => command(args),
             None => println!("{}: command not found", input.trim()),
         }
     }
