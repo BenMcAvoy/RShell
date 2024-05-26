4705bc2f27ca86dba2a3a346b4bdf5262b36fc39
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-05-26 02:08:47 +0100

    feat: implement command not found message

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-#[allow(unused_imports)]
+#![allow(unused_imports)]
+
+use std::collections::HashMap;
 use std::io::{self, Write};
 
-fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+macro_rules! printnnl {($($arg:tt)*) => {print!($($arg)*);io::stdout().flush().unwrap();}}
 
-    // Wait for user input
+fn main() {
+    let commands: HashMap<&str, Box<dyn Fn(Vec<&str>) -> ()>> = HashMap::new();
+
+    printnnl!("$ ");
+
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    match commands.get(input.trim()) {
+        Some(command) => command(vec![]),
+        None => println!("{}: command not found", input.trim()),
+    }
 }
