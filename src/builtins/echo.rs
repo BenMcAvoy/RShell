ea6e65b2b998fedc554673cc4adaa9b2ea033c94
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-05-26 22:24:12 +0100

    feat: replace variable in echo with values

--- src/builtins/echo.rs
+++ src/builtins/echo.rs
@@ -1,6 +1,23 @@
 use crate::types::Args;
 
 pub fn echo(args: Args) {
-    let args = args.list[1..].join(" ");
-    println!("{args}");
+    let mut list: Vec<String> = args.list.iter().map(|v| v.to_string()).collect();
+
+    for item in list.iter_mut() {
+        if item.starts_with('$') {
+            let var_name = item[1..].to_string();
+            let var_value = match std::env::var(&var_name) {
+                Ok(v) => v,
+                Err(_) => {
+                    eprintln!("echo: ${}: not set", var_name);
+                    return;
+                }
+            };
+
+            *item = var_value;
+        }
+    }
+
+    let args = list[1..].join(" ");
+    println!("{}", args);
 }
